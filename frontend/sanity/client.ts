fa1bf75b5804b83f9e0cbdff457e42200bcdfc8f
Jatin-Rai <jatinrai258@gmail.com>
2023-11-07 08:44:09 +0530

    projectid fixed

--- frontend/sanity/client.ts
+++ frontend/sanity/client.ts
@@ -2,13 +2,11 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-
-const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-11-04";
-
-const token = process.env.NEXT_PUBLIC_SANITY_TOKEN;
+const projectId: string | undefined = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset: string | undefined = process.env.NEXT_PUBLIC_SANITY_DATASET;
+const apiVersion: string =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-11-07";
+const token: string | undefined = process.env.NEXT_PUBLIC_SANITY_TOKEN;
 
 export const client = createClient({
   projectId,
