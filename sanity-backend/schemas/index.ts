5cc644a7558f8e7cf06b381c18807ad79c821a2c
Jatin-Rai <jatinrai258@gmail.com>
2024-04-29 22:15:11 +0530

    removed: unused schemas renamed: works to projects

--- sanity-backend/schemas/index.ts
+++ sanity-backend/schemas/index.ts
@@ -1,21 +1,13 @@
 import about from './abouts'
-import brands from './brands'
-import contact from './contact'
 import experiences from './experiences'
 import skills from './skills'
-import testimonials from './testimonials'
 import workExperience from './workExperience'
-import works from './works'
-import blogs from './blogs'
+import projects from './projects'
 
 export const schemaTypes = [
   about,
-  brands,
-  contact,
-  blogs,
   experiences,
   skills,
-  testimonials,
   workExperience,
-  works,
+  projects,
 ]
