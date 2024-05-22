be0c7f3b9947447f176aa56b086f042333451713
Jatin-Rai <jatinrai258@gmail.com>
2024-05-22 12:50:28 +0530

    fixed: experience error

--- frontend/sections/Skills/Skills.tsx
+++ frontend/sections/Skills/Skills.tsx
@@ -76,7 +76,7 @@ const Skills: React.FC = () => {
           ))}
         </motion.div>
         <div className="app__skills-exp">
-          {experiences.map((experience) => (
+          {experiences?.map((experience) => (
             <motion.div className="app__skills-exp-item" key={experience.year}>
               <div className="app__skills-exp-year">
                 <p className="bold-text">{experience.year}</p>
