19e6d57317e99d35583d03046c6315eaf2fa4906
Jatin-Rai <jatinrai258@gmail.com>
2024-07-03 15:44:43 +0530

    added: dev.to profile link

--- frontend/components/SocialMedia.tsx
+++ frontend/components/SocialMedia.tsx
@@ -1,20 +1,10 @@
 import React from "react";
 
-// import { BsInstagram } from "react-icons/bs";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaDev } from "react-icons/fa";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
-      {/* <div>
-        <a
-          href="https://www.instagram.com/jatin.j.rai"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <BsInstagram />
-        </a>
-      </div> */}
       <div>
         <a
           href="https://linkedin.com/in/jatinrai96"
@@ -31,6 +21,15 @@ const SocialMedia = () => {
           rel="noopener noreferrer"
         >
           <FaGithub />
+        </a>
+      </div>
+      <div>
+        <a
+          href="https://dev.to/jatinrai"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaDev />
         </a>
       </div>
     </div>
